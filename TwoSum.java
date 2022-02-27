8b48e3bcc2d50bd4d26e9f22bd9dbb3c8323638a
Jason Zhu <94236556+JZ-FSDev@users.noreply.github.com>
2022-02-26 21:29:49 -0600

    Update TwoSum.java

--- TwoSum.java
+++ TwoSum.java
@@ -1,3 +1,10 @@
+ /**
+ * Self-written "Two Sum" from LeetCode solution.
+ *
+ * @author JZ-FSDev
+ * @since 17.0.1
+ * @version 0.0.1
+ */
 public class TwoSum {
 
     public static void main(String[] args) {
