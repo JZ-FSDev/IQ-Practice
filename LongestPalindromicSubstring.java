326e484c3cde88e1facb0f4702d0bf4d6055f475
Jason Zhu <94236556+JZ-FSDev@users.noreply.github.com>
2022-02-27 22:34:52 -0600

    Update LongestPalindromicSubstring.java

--- LongestPalindromicSubstring.java
+++ LongestPalindromicSubstring.java
@@ -1,10 +1,24 @@
+ /**
+ * Self-written solution of "Longest Palindromic Substring" from LeetCode.
+ *
+ * @author JZ-FSDev
+ * @since 17.0.1
+ * @version 0.0.1
+ */
 public class LongestPalindromicSubstring {
     
     public static void main(String[] args) {
-        String s = "";
+        // Test the method
+        String s = "asdsa";
         System.out.println(longestPalindrome(s));
     }
 
+    /**
+     * Given a string s, return the longest palindromic substring in s.
+     * 
+     * @param s The String to check for the longest palindrome substring.
+     * @return Returns the longest palindrome substring of the specified String.
+     */
     public static String longestPalindrome(String s) {
         String ans = "";
 
@@ -23,7 +37,12 @@ public class LongestPalindromicSubstring {
         return ans;
     }
 
-  
+    /**
+     * Returns true if the specified String is a palindrome.
+     * 
+     * @param s The String to be checked if it is a palindrome.
+     * @return Returns true if the specified String is a palindrome.
+     */
     private static boolean palindrome(String s){
         int start = 0, end = s.length()-1;
         boolean result = true;
