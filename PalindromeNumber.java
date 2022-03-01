e023133ef50dda4fdba89239066310e0f3899fc2
Jason Zhu <94236556+JZ-FSDev@users.noreply.github.com>
2022-02-28 22:00:42 -0600

    Update PalindromeNumber.java

--- PalindromeNumber.java
+++ PalindromeNumber.java
@@ -1,10 +1,24 @@
+ /**
+ * Self-written solution of "Palindrome Number" from LeetCode.
+ *
+ * @author JZ-FSDev
+ * @since 17.0.1
+ * @version 0.0.1
+ */
 public class PalindromeNumber {
     
     public static void main(String[] args) {
+        // Test the method
         System.out.println(isPalindrome(010));
     }
 
-    
+    /**
+     * Given an integer x, return true if x is palindrome integer.
+     * An integer is a palindrome when it reads the same backward as forward.
+     * 
+     * @param x The int to check if it is a palindrome.
+     * @return Returns true if the specified int is a palindrome.
+     */
     public static boolean isPalindrome(int x) {
         boolean result = true;
         if(x < 0){
