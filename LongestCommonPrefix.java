a105e56fe111b617659de4592a24692a961630a7
Jason Zhu <94236556+JZ-FSDev@users.noreply.github.com>
2022-03-12 23:03:11 -0600

    Update LongestCommonPrefix.java

--- LongestCommonPrefix.java
+++ LongestCommonPrefix.java
@@ -1,10 +1,25 @@
+ /**
+ * Self-written solution for "Longest Common Prefix" from LeetCode.
+ *
+ * @author JZ-FSDev
+ * @since 17.0.1
+ * @version 0.0.1
+ */
 public class LongestCommonPrefix {
     
     public static void main(String[] args) {
+        // Test the method
         String[] s = {"flower","flower","flower","flower"};
         System.out.println(longestCommonPrefix(s));
     }
 
+    /**
+     * Write a function to find the longest common prefix string amongst an array of strings.
+     * If there is no common prefix, return an empty string "".
+     * 
+     * @param strs The array of Strings to have the longest common prefix determined.
+     * @return The longest common prefix of all the Strings in the specified String array.
+     */
     public static String longestCommonPrefix(String[] strs) {
         String prefix = "";
         boolean consistent = true;
