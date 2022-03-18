258dd09763977f3d0284ec771dbe8991cbefb432
Jason Zhu <94236556+JZ-FSDev@users.noreply.github.com>
2022-03-18 12:46:36 -0500

    Update ThreeSum.java

--- ThreeSum.java
+++ ThreeSum.java
@@ -1,4 +1,5 @@
 import java.util.List;
+import java.util.ArrayList;
 
 public class ThreeSum {
     
@@ -12,6 +13,23 @@ public class ThreeSum {
      * @return
      */
     public static List<List<Integer>> threeSum(int[] nums) {
-        
+        List<List<Integer>> list = new ArrayList<List<Integer>>();
+
+        if(nums.length > 3){
+            int one = 0;
+            int two = 1;
+            int three = 2;
+
+            while(one != nums.length - 3 && two != nums.length - 2){
+                if(nums[one] + nums[two] + nums[three] == 0){
+                    List<Integer> temp = new ArrayList<Integer>();
+                    temp.add(nums[one]);
+                    temp.add(nums[two]);
+                    temp.add(nums[three]);
+                }
+            }
+        }
+    
+        return list;
     }
 }
