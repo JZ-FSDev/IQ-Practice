7f98f5115945b6a3a6865174337a41d264d09dc5
Jason Zhu <94236556+JZ-FSDev@users.noreply.github.com>
2022-03-24 12:43:02 -0500

    Update LetterCombPhoneNumber.java

--- LetterCombPhoneNumber.java
+++ LetterCombPhoneNumber.java
@@ -9,6 +9,50 @@ public class LetterCombPhoneNumber {
            System.out.print(s + " ");
        }
     }
+    
+        public static List<String> letterCombinations(String digits) {
+        List<String> result = new ArrayList<String>();
+        char[] group1, group2, group3, group4;
+        if(digits.length() == 1){
+            group1 = convert(Integer.parseInt(digits));
+            for(int i = 0; i < group1.length; i++){
+                result.add("" + group1[i]);
+            }
+        }else if(digits.length() == 2){
+            group1 = convert(Integer.parseInt(digits.substring(0, 1)));
+            group2 = convert(Integer.parseInt(digits.substring(1, 2)));
+            for(int i = 0; i < group1.length; i++){
+                for(int j = 0; j < group2.length; j++){
+                    result.add("" + group1[i]);
+                }
+            }
+            for(int i = 0; i < group1.length * group2.length; i++){
+                result.set(i, result.get(i) + group2[i % group2.length]);
+            }
+        }else if(digits.length() == 3){
+            group1 = convert(Integer.parseInt(digits.substring(0, 1)));
+            group2 = convert(Integer.parseInt(digits.substring(1, 2)));
+            group3 = convert(Integer.parseInt(digits.substring(2, 3)));
+            for(int i = 0; i < group1.length; i++){
+                for(int j = 0; j < group2.length * group3.length; j++){
+                    result.add("" + group1[i]);
+                }
+            }
+            int count = 0;
+            int index = 0;
+            for(int j = 0; j < group1.length * group2.length; j++){
+                for(int i = 0; i < group3.length; i++){
+                    result.set(index, result.get(index) + group2[count % group2.length]);
+                    index++;
+                }
+                count++;
+            }
+            for(int i = 0; i < group1.length * group2.length * group3.length; i++){
+                result.set(i, result.get(i) + group3[i % group3.length]);
+            }
+        }
+        return result;
+    }
 
     private static char[] convert(int i){
         if(i == 2){
