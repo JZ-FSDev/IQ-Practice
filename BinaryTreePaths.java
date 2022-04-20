5f8a94785708e1c3b0577ff021335574c968ada3
Jason Zhu <94236556+JZ-FSDev@users.noreply.github.com>
2022-04-20 13:49:00 -0500

    Create BinaryTreePaths.java

---
+++ BinaryTreePaths.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTreePaths {
+
+    public static void main(String[] args) {
+        TreeNode one = new TreeNode(1);
+        TreeNode two = new TreeNode(2);
+        TreeNode three = new TreeNode(3);
+        TreeNode five = new TreeNode(5);
+        one.left = two;
+        one.right = three;
+        two.right = five;
+        print(binaryTreePaths(one));
+    }
+}    
+   
