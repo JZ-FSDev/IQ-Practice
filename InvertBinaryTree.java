26b0e772307a396f4a6cda7375e68dc5dc941623
Jason Zhu <94236556+JZ-FSDev@users.noreply.github.com>
2022-04-20 14:16:31 -0500

    Update InvertBinaryTree.java

--- InvertBinaryTree.java
+++ InvertBinaryTree.java
@@ -1,12 +1,32 @@
+/**
+ * Self-written solution of "Invert Binary Tree" from LeetCode.
+ * Faster than 100.00% of Java online submissions.
+ * Less than 76.24% of Java online submissions.
+ *
+ * @author JZ-FSDev
+ * @since 17.0.1
+ * @version 0.0.1
+ */
 public class InvertBinaryTree {
 
+    /**
+     * Given the root of a binary tree, invert the tree, and return its root.
+     * 
+     * @param root The root of the binary tree.
+     * @return The root of the binary tree post inversion.
+     */
     public TreeNode invertTree(TreeNode root) {
         invertTreeHelper(root);
         return root;
     }
 
-    private void invertTreeHelper(TreeNode root){
-        if(root != null){
+    /**
+     * Helper method to invert a given binary tree of its root.
+     * 
+     * @param root The root of the binary tree to invert.
+     */
+    private void invertTreeHelper(TreeNode root) {
+        if (root != null) {
             TreeNode temp = root.left;
             root.left = root.right;
             root.right = temp;
