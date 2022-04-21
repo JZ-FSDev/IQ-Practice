3204ef5fe598144ddc4502c5335f6de43e17549a
Jason Zhu <94236556+JZ-FSDev@users.noreply.github.com>
2022-04-21 11:42:26 -0500

    Update MaximumDepthOfBinaryTree.java

--- MaximumDepthOfBinaryTree.java
+++ MaximumDepthOfBinaryTree.java
@@ -1,11 +1,34 @@
+/**
+ * Self-written solution for "Longest Substring Without Repeating Characters"
+ * from LeetCode.
+ *
+ * @author JZ-FSDev
+ * @since 17.0.1
+ * @version 0.0.1
+ */
 public class MaximumDepthOfBinaryTree {
+
+    /**
+     * Given the root of a binary tree, return its maximum depth.
+     * A binary tree's maximum depth is the number of nodes along the longest path
+     * from the root node down to the farthest leaf node.
+     * 
+     * @param root The root of the binary tree.
+     * @return The maximum depth of the given binary tree.
+     */
     public int maxDepth(TreeNode root) {
         return inOrderTraversal(root) - 1;
     }
-    
-    private int inOrderTraversal(TreeNode curr){
+
+    /**
+     * Helper method to return the max depth + 1 of the given binary tree.
+     * 
+     * @param curr The current TreeNode in each recursive call.
+     * @return The max depth + 1 of the given binary tree.
+     */
+    private int inOrderTraversal(TreeNode curr) {
         int left = 1, right = 1;
-        if(curr != null){
+        if (curr != null) {
             left += inOrderTraversal(curr.left);
             right += inOrderTraversal(curr.right);
         }
