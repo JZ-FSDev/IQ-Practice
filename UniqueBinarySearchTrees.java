be8b0f8b9d4f1c39439174eb4bdb8526c69cbc7e
Jason Zhu <94236556+JZ-FSDev@users.noreply.github.com>
2022-04-22 12:31:23 -0500

    Update UniqueBinarySearchTrees.java

--- UniqueBinarySearchTrees.java
+++ UniqueBinarySearchTrees.java
@@ -1,18 +1,45 @@
 import java.math.BigInteger;
 
-public class UniqueBinarySearchTrees{
+ /**
+ * Self-written solution of "Unique Binary Search Trees" from LeetCode.
+ *
+ * @author JZ-FSDev
+ * @since 17.0.1
+ * @version 0.0.1
+ */
+public class UniqueBinarySearchTrees {
 
+    /**
+     * Given an integer n, return the number of structurally unique BST's (binary
+     * search trees) which has exactly n nodes of unique values from 1 to n.
+     * 
+     * @param n The number of vertices in the tree.
+     * @return The number of structurally unique BSTs.
+     */
     public int numTrees(int n) {
-        return (choose(2 * n, n).divide(BigInteger.valueOf(n+1))).intValue();
+        return (choose(2 * n, n).divide(BigInteger.valueOf(n + 1))).intValue();
     }
 
-    private BigInteger choose(int n, int r){
-        return factorial(n).divide(factorial(r).multiply(factorial(n-r)));
+    /**
+     * Returns the value of n choose r.
+     * 
+     * @param n The number of items to choose from.
+     * @param r The number of items to choose from n.
+     * @return The value of n choose r.
+     */
+    private BigInteger choose(int n, int r) {
+        return factorial(n).divide(factorial(r).multiply(factorial(n - r)));
     }
 
-    private BigInteger factorial(int n){
+    /**
+     * Returns the value of n factorial.
+     * 
+     * @param n The number of the factorial to return.
+     * @return The value of n factorial.
+     */
+    private BigInteger factorial(int n) {
         BigInteger result = new BigInteger("1");
-        for(int i = n; i > 1; i--){
+        for (int i = n; i > 1; i--) {
             result = result.multiply(BigInteger.valueOf(i));
         }
         return result;
