3e15d07fe4fba6efcd3514b1a0b16c68bd40b51b
Jason Zhu <94236556+JZ-FSDev@users.noreply.github.com>
2022-04-26 21:39:10 -0500

    Update FindIfPathExistsInGraph.java

--- FindIfPathExistsInGraph.java
+++ FindIfPathExistsInGraph.java
@@ -3,22 +3,20 @@ import java.util.Queue;
 
 public class FindIfPathExistsInGraph {
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-        int[] path = new int[n*3];
-        int items = 0;
+        boolean[] path = new boolean[n];
         Queue<Integer> queue = new LinkedList<Integer>();
         boolean d = (source == destination);
         queue.add(source);
         while(!queue.isEmpty() && !d){
             int temp = queue.remove();
-            path[items] = temp;
-            items++;
-            for(int i = 0; i < edges.length; i++){
-                if(edges[i][0] == temp && !exists(path, items, edges[i][1])){
+            path[temp] = true;
+            for(int i = 0; i < edges.length && !d; i++){
+                if(edges[i][0] == temp && !path[edges[i][1]]){
                     queue.add(edges[i][1]);
                     if(edges[i][1] == destination){
                         d = true;
                     }
-                }else if(edges[i][1] == temp && !exists(path, items, edges[i][0])){
+                }else if(edges[i][1] == temp && !path[edges[i][0]]){
                     queue.add(edges[i][0]);
                     if(edges[i][0] == destination){
                         d = true;
@@ -28,14 +26,4 @@ public class FindIfPathExistsInGraph {
         }
         return d;
     }
-    
-    private boolean exists(int[] list, int size, int item) {
-        boolean exists = false;
-        for (int i = 0; i < size; i++) {
-            if (list[i] == item) {
-                exists = true;
-            }
-        }
-        return exists;
-    }    
 }
