86fe6f0f1d8009476c24835b6ae0f2add751d5e9
Jason Zhu <94236556+JZ-FSDev@users.noreply.github.com>
2022-05-03 14:47:37 -0500

    Update MaximumSubarray.py

--- MaximumSubarray.py
+++ MaximumSubarray.py
@@ -1,10 +1,18 @@
+# Self-written solution of "Maximum Subarray" from LeetCode.
+# Faster than 85.52% of Python online submissions.
+# author JZ-FSDev
+
 import sys
 
+# Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+# A subarray is a contiguous part of an array.
 def maxSubArray(self, nums):
     """
     :type nums: List[int]
     :rtype: int
     """
+    # Kadane's algorithm
+
     currMax = 0
     globalMax = -sys.maxint-1
     for n in nums:
