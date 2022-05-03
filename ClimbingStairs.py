bba4f3a792d918e29215d800cf0e70bce20f6660
Jason Zhu <94236556+JZ-FSDev@users.noreply.github.com>
2022-05-03 13:13:20 -0500

    Update ClimbingStairs.py

--- ClimbingStairs.py
+++ ClimbingStairs.py
@@ -1,6 +1,11 @@
+# Self-written solution of "Climbing Stairs" from LeetCode.
+# Faster than 97.31% of Python online submissions.
+# author JZ-FSDev
+
 import math
 
-
+# You are climbing a staircase. It takes n steps to reach the top.
+# Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
 def climbStairs(n):
     """
     :type n: int
