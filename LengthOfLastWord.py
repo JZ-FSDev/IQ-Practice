66c3c4d280da0e00f055bbd722426b030abaee25
Jason Zhu <94236556+JZ-FSDev@users.noreply.github.com>
2022-05-05 11:32:58 -0500

    Update LengthOfLastWord.py

--- LengthOfLastWord.py
+++ LengthOfLastWord.py
@@ -1,3 +1,9 @@
+# Self-written solution of "Length of Last Word" from LeetCode.
+# Faster than 89.05%% of Python online submissions.
+# author JZ-FSDev
+
+# Given a string s consisting of some words separated by some number of spaces, return the length of the last word in the string.
+# A word is a maximal substring consisting of non-space characters only.
 def lengthOfLastWord(self, s):
     """
     :type s: str
