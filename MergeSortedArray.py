fa723d570bd193df3fe7bbbf3276209e5b976e0f
Jason Zhu <94236556+JZ-FSDev@users.noreply.github.com>
2022-05-06 23:56:06 -0500

    Update MergeSortedArray.py

--- MergeSortedArray.py
+++ MergeSortedArray.py
@@ -1,5 +1,5 @@
 # Self-written solution of "Merge Sorted Array" from LeetCode.
-# Faster than #87.95% of Python online submissions.
+# Faster than 87.95% of Python online submissions.
 # author JZ-FSDev
 
 # You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
