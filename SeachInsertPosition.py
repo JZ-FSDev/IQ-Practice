90274949c823149b8fa209766c73b26cfd0a47ee
Jason Zhu <94236556+JZ-FSDev@users.noreply.github.com>
2022-05-09 12:22:35 -0500

    Update SeachInsertPosition.py

--- SeachInsertPosition.py
+++ SeachInsertPosition.py
@@ -1,4 +1,9 @@
+# Self-written solution of "Search Insert Position" from LeetCode.
+# Faster than 73.02% of Python online submissions.
+# author JZ-FSDev
 
+# Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+# You must write an algorithm with O(log n) runtime complexity.
 def searchInsert(self, nums, target):
     """
     :type nums: List[int]
