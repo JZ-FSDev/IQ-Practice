3330d6d79cb82519287abfb9b2e385237fce390f
Jason Zhu <94236556+JZ-FSDev@users.noreply.github.com>
2022-05-10 11:20:03 -0500

    Update CombinationSum.py

--- CombinationSum.py
+++ CombinationSum.py
@@ -1,3 +1,10 @@
+# Self-written solution of "Combination Sum" from LeetCode.
+# Faster than 61.38% of Python online submissions.
+# author JZ-FSDev
+
+# Given an array of distinct integers candidates and a target integer target, return a list of all unique combinations of candidates where the chosen numbers sum to target. You may return the combinations in any order.
+# The same number may be chosen from candidates an unlimited number of times. Two combinations are unique if the frequency of at least one of the chosen numbers is different.
+# It is guaranteed that the number of unique combinations that sum up to target is less than 150 combinations for the given input.
 def combinationSum(self, candidates, target):
     """
     :type candidates: List[int]
