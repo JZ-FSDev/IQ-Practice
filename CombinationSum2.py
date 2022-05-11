c13139eb7b6461e4f2dafed3b2a756935bc88434
Jason Zhu <94236556+JZ-FSDev@users.noreply.github.com>
2022-05-11 15:05:13 -0500

    Update CombinationSum2.py

--- CombinationSum2.py
+++ CombinationSum2.py
@@ -1,3 +1,10 @@
+# Self-written solution of "Combination Sum2" from LeetCode.
+# Can be more efficient (exceeds time limit, but works)
+# author JZ-FSDev
+
+# Given a collection of candidate numbers (candidates) and a target number (target), find all unique combinations in candidates where the candidate numbers sum to target.
+# Each number in candidates may only be used once in the combination.
+# Note: The solution set must not contain duplicate combinations.
 def combinationSum2(self, candidates, target):
     """
     :type candidates: List[int]
