1d1b9ea6aee9e1ac29a447afdd228766a3efa864
Jason Zhu <94236556+JZ-FSDev@users.noreply.github.com>
2022-05-17 20:58:36 -0500

    Update ValidSudoku.py

--- ValidSudoku.py
+++ ValidSudoku.py
@@ -1,5 +1,14 @@
+# Self-written solution of "Valid Sudoku" from LeetCode.
+# Faster than 67.71% of Python online submissions.
+# author JZ-FSDev
 
-
+# Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+# Each row must contain the digits 1-9 without repetition.
+# Each column must contain the digits 1-9 without repetition.
+# Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+# Note:
+# A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+# Only the filled cells need to be validated according to the mentioned rules.
 def isValidSudoku(self, board):
     """
     :type board: List[List[str]]
