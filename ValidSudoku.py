11606320ba0b0932b5e63c8c28327fd74a2960c8
Jason Zhu <94236556+JZ-FSDev@users.noreply.github.com>
2022-05-17 20:40:01 -0500

    Update ValidSudoku.py

--- ValidSudoku.py
+++ ValidSudoku.py
@@ -5,9 +5,14 @@ def isValidSudoku(self, board):
     """
     n = int(math.sqrt(len(board)))
     
+    def rowColValid(row, col):
+        for i in range(0, len(board)):
+            if (board[i][col] == board[row][col] and i != row) or (board[row][i] == board[row][col] and i != col):
+                return False
+        return True
+    
     for r in range(0, len(board)):
         for c in range(0, len(board[r])):
             if (board[r][c] != ".") and (not cellValid(r,c) or not rowColValid(r,c)):
                 return False
     return True
-  
