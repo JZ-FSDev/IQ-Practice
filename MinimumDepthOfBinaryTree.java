cf124569fbee4e531015eafdad39b1e01dc75c8b
Jason Zhu <94236556+JZ-FSDev@users.noreply.github.com>
2022-05-20 22:26:18 -0500

    Update MinimumDepthOfBinaryTree.java

--- MinimumDepthOfBinaryTree.java
+++ MinimumDepthOfBinaryTree.java
@@ -1,21 +1,35 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class MinimumDepthOfBinaryTree{
 
     public int minDepth(TreeNode root) {
-        Integer min = new Integer(Integer.MAX_VALUE);
-        minDepth(root, min);
-        return min.intValue();
-    }
-    
-    public int minDepth(TreeNode root, Integer minDepth){
-        int node = 1;
-        if(root != null){
-            if(root.left == null && root.right == null && node < minDepth.intValue()){
-                minDepth = node;
+        int depth = 1;
+        if (root == null) {
+            depth = 0;
+        }else{
+            Queue<TreeNode> q = new LinkedList<>();
+            q.add(root);
+            boolean terminate = false;
+            while (!q.isEmpty() && !terminate) {
+                int size = q.size();
+                for (int i = 0; i < size; i++) {
+                    TreeNode curr = q.poll();
+                    if (curr.left == null && curr.right == null && !terminate) {
+                        terminate = true;
+                        depth --;
+                    }
+                    if (curr.left != null) {
+                        q.offer(curr.left);
+                    }
+                    if (curr.right != null) {
+                        q.offer(curr.right);
+                    }
+                }
+                depth++;
             }
-            node += minDepth(root.left, minDepth);
-            node += minDepth(root.right, minDepth);
         }
-        return node;
+        return depth;
     }
 
 }
