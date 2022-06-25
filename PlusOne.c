f3ee70d68c51176fc7e7910fd5efba069741e060
Jason Zhu <94236556+JZ-FSDev@users.noreply.github.com>
2022-06-24 23:58:44 -0500

    Update PlusOne.c

--- PlusOne.c
+++ PlusOne.c
@@ -1,3 +1,14 @@
-int* plusOne(int* digits, int digitsSize, int* returnSize){
-
+bool isPowerOfThree(int n){
+    bool isPower = false;
+    if(n == 1){
+        isPower = true;
+    }
+    while(n >= 3 && !isPower && n % 3 == 0){
+        if((n / 3) == 1){
+            isPower = true;
+        }else{
+            n /= 3;
+        }
+    }
+    return isPower;
 }
