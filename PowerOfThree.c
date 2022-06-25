1b49d3e90608853292531986b7b30729ee803ee6
Jason Zhu <94236556+JZ-FSDev@users.noreply.github.com>
2022-06-25 00:09:52 -0500

    Update and rename PlusOne.c to PowerOfThree.c

--- PowerOfThree.c
+++ PowerOfThree.c
@@ -1,3 +1,7 @@
+// Runtime: 15 ms, faster than 96.80% of C online submissions for Power of Three.
+
+// Given an integer n, return true if it is a power of three. Otherwise, return false.
+// An integer n is a power of three, if there exists an integer x such that n == 3x.
 bool isPowerOfThree(int n){
     bool isPower = false;
     if(n == 1){
