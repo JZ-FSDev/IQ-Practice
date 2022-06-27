9164f44a527796c29099888751dc7e3618da2597
Jason Zhu <94236556+JZ-FSDev@users.noreply.github.com>
2022-06-26 23:23:42 -0500

    Create ReverseString.c

---
+++ ReverseString.c
@@ -0,0 +1,14 @@
+//@author JZ-FSDev
+//Runtime: 54 ms, faster than 93.12% of C online submissions for Reverse String.
+
+// Write a function that reverses a string. The input string is given as an array of characters s.
+// You must do this by modifying the input array in-place with O(1) extra memory.
+
+void reverseString(char* s, int sSize){
+    char c;
+    for(int i = 0; i < sSize / 2; i++){
+        c = s[i];
+        s[i] = s[sSize - i - 1];
+        s[sSize - i - 1] = c;
+    }
+}
