f0f84a943f7c1ab66874e59e1e4b261722ee8721
Jason Zhu <94236556+JZ-FSDev@users.noreply.github.com>
2022-07-02 13:19:02 -0500

    Update MaxConsecutiveOnes.c

--- MaxConsecutiveOnes.c
+++ MaxConsecutiveOnes.c
@@ -1,5 +1,21 @@
+// @author JZ-FSDev
+// Runtime: 47 ms, faster than 82.46% of C online submissions for Max Consecutive Ones.
 
-
+// Given a binary array nums, return the maximum number of consecutive 1's in the array.
 int findMaxConsecutiveOnes(int* nums, int numsSize){
-
+    int max = 0;
+    int temp = 0;
+    int i = 0;
+    for(i = 0; i < numsSize; i++){
+        if(nums[i] == 0){
+            if(i - temp > max){
+                max = i - temp;
+            }
+            temp = i + 1;
+        }
+    }
+    if(i - temp > max){
+        max = i - temp;
+    }
+    return max;
 }
