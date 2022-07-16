cffab558168063bbe7cb83a18fb5b70f565a5e57
Jason Zhu <94236556+JZ-FSDev@users.noreply.github.com>
2022-07-15 23:31:18 -0500

    Update DiameterOfBinaryTree.c

--- DiameterOfBinaryTree.c
+++ DiameterOfBinaryTree.c
@@ -7,7 +7,23 @@
  * };
  */
 
+int max(int a, int b){
+    return (a >= b) ? a : b; 
+}
+
+int solve(struct TreeNode* root, int *result){
+    if(root == NULL) return 0;
+    int leftHeight = solve(root->left, result);
+    int rightHeight = solve(root->right, result);
+    int temp = 1 + max(leftHeight, rightHeight);
+    *result = max(*result, leftHeight + rightHeight);
+    return temp;
+}
+
 
 int diameterOfBinaryTree(struct TreeNode* root){
-
+    int result = INT_MIN;
+    solve(root, &result);
+    return result;
 }
+
