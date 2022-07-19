cdb2592aae2462581a82adbe2b738d4222866680
Jason Zhu <94236556+JZ-FSDev@users.noreply.github.com>
2022-07-18 23:14:36 -0500

    Update FibonacciNumber.c

--- FibonacciNumber.c
+++ FibonacciNumber.c
@@ -1,5 +1,12 @@
 
-
 int fib(int n){
-    
+    if(n == 0) return 0;
+    if(n == 1) return 1;
+    int sol[n + 1];
+    sol[0] = 0;
+    sol[1] = 1;
+    for(int  i = 2; i <= n; i++){
+        sol[i] = sol[i - 1] + sol[i - 2];
+    }
+    return sol[n];
 }
