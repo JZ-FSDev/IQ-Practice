3a29de633d9a9b468ebee601467e73d1b3aba8de
Jason Zhu <94236556+JZ-FSDev@users.noreply.github.com>
2022-07-19 23:49:07 -0500

    Update FibonacciNumber.c

--- FibonacciNumber.c
+++ FibonacciNumber.c
@@ -1,3 +1,10 @@
+// @author JZ-FSDev
+// Runtime: 0 ms, faster than 100.00% of C online submissions for Fibonacci Number.
+
+// The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+// F(0) = 0, F(1) = 1
+// F(n) = F(n - 1) + F(n - 2), for n > 1.
+// Given n, calculate F(n).
 
 int fib(int n){
     if(n == 0) return 0;
