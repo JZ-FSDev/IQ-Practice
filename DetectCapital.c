43a2c890d2f68e995d759c154d081caad27ebb9d
Jason Zhu <94236556+JZ-FSDev@users.noreply.github.com>
2022-07-25 06:40:05 -0500

    Update DetectCapital.c

--- DetectCapital.c
+++ DetectCapital.c
@@ -1,3 +1,13 @@
+// @author JZ-FSDev
+// Runtime: 0 ms, faster than 100.00% of C online submissions for Detect Capital.
+
+// We define the usage of capitals in a word to be right when one of the following cases holds:
+
+// All letters in this word are capitals, like "USA".
+// All letters in this word are not capitals, like "leetcode".
+// Only the first letter in this word is capital, like "Google".
+// Given a string word, return true if the usage of capitals in it is right.
+
 bool detectCapitalUse(char * word){
     bool proper = true;
     for(int i = 0; i < strlen(word) && proper; i++){
