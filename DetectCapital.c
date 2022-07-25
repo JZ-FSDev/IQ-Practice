5a2acb709eaff662863873e770416ddf3bd4102f
Jason Zhu <94236556+JZ-FSDev@users.noreply.github.com>
2022-07-24 23:41:29 -0500

    Update DetectCapital.c

--- DetectCapital.c
+++ DetectCapital.c
@@ -1,5 +1,19 @@
-
-
 bool detectCapitalUse(char * word){
-
+    bool proper = true;
+    for(int i = 0; i < strlen(word) && proper; i++){
+        if(isupper(word[i])){
+            if(i >= 1){
+                int j;
+                for(j = 0; j <= i && proper; j++){
+                    if(!isupper(word[j])){
+                        proper = false;
+                    }
+                }
+                if(j < strlen(word) && isupper(word[j - 1]) && !isupper(word[j])){
+                    proper = false;
+                }
+            }
+        }
+    }
+    return proper;
 }
