e1aedb81fa86145b23bd22390a233702ce15b262
Jason Zhu <94236556+JZ-FSDev@users.noreply.github.com>
2022-07-27 10:16:27 -0500

    Update LongestHarmoniousSubsequence.java

--- CombinationSum2.py
+++ CombinationSum2.py
@@ -5,21 +5,25 @@
 # Given a collection of candidate numbers (candidates) and a target number (target), find all unique combinations in candidates where the candidate numbers sum to target.
 # Each number in candidates may only be used once in the combination.
 # Note: The solution set must not contain duplicate combinations.
-def combinationSum2(self, candidates, target):
-    """
-    :type candidates: List[int]
-    :type target: int
-    :rtype: List[List[int]]
-    """
-    res = []
-    candidates.sort()
 
-    def dfs(target, index, path):
-        if target == 0 and path not in res:
-            res.append(path)
-        elif target > 0:
-            for i in range(index + 1, len(candidates)): # for unique combinations
-                dfs(target - candidates[i], i, path + [candidates[i]]) #adds cand at index i as a list to end of path list
-                
-    dfs(target, -1, [])
-    return res    
+class Solution:
+    def combinationSum2(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        res = []
+        candidates.sort()
+
+        def dfs(target, index, path):
+            if target == 0 and path not in res:
+                res.append(path)
+            elif target > 0:
+                for i in range(index, len(candidates)): # for unique combinations
+                    if i > index and candidates[i] == candidates[i-1]:
+                        continue
+                    dfs(target - candidates[i], i + 1, path + [candidates[i]]) # adds cand at index i as a list to end of path list
+        
+        dfs(target, 0, [])
+        return res   
--- LongestHarmoniousSubsequence.java
+++ LongestHarmoniousSubsequence.java
@@ -2,19 +2,31 @@ class Solution {
     public int findLHS(int[] nums) {
         Arrays.sort(nums);
         int len = 0;
-        int temp = 0;
+        int temp;
+        int count;
         for(int i = 0; i < nums.length; i++){
-            temp = 0;
+            temp = 2;
+            count = 0;
             for(int j = i + 1; j < nums.length; j++){
+                if(count >= 2 && nums[i] == nums[j]){
+                    count++;
+                }else if(nums[i] == nums[j]){
+                    count = 2;
+                    temp--;
+                }
                 if(Math.abs(nums[j] - nums[i]) == 1){
-                    temp++;
-                }else{
+                    for(int k = j + 1; k < nums.length; k++){
+                        if(nums[k] == nums[j]){
+                            temp++;
+                        }
+                    }
+                    if(temp + count > len){
+                        len = temp + count;
+                    }
                     break;
                 }    
             }
-            if(temp > len){
-                len = temp;
-            }
+
         }
         return len;
     }
