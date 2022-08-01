de39fb08cf00f0f73b776be1ddef264961670505
Jason Zhu <94236556+JZ-FSDev@users.noreply.github.com>
2022-07-31 23:24:48 -0500

    Update ArrayPartition.c

--- ArrayPartition.c
+++ ArrayPartition.c
@@ -2,29 +2,63 @@ int min(int a, int b){
     return a <= b ? a : b;
 }
 
-void sort(int nums[], int output[], int numLength){
-    int min = INT_MAX;
-    int temp = -1;
-    int temptemp;
-    for(int i = 0; i < numLength; i++){
-        min = INT_MAX;
-        temp = -1;
-        for(int k = i; k < numLength; k++){
-            if(nums[k] < min){
-                min = nums[k];
-                temp = k;
-            }
-        }
-        temptemp = nums[i];
-        nums[i] = nums[temp];
-        nums[temp] = temptemp;
+// function to swap elements
+void swap(int *a, int *b) {
+  int t = *a;
+  *a = *b;
+  *b = t;
+}
+
+// function to find the partition position
+int partition(int array[], int low, int high) {
+  
+  // select the rightmost element as pivot
+  int pivot = array[high];
+  
+  // pointer for greater element
+  int i = (low - 1);
+
+  // traverse each element of the array
+  // compare them with the pivot
+  for (int j = low; j < high; j++) {
+    if (array[j] <= pivot) {
+        
+      // if element smaller than pivot is found
+      // swap it with the greater element pointed by i
+      i++;
+      
+      // swap element at i with element at j
+      swap(&array[i], &array[j]);
     }
+  }
+
+  // swap the pivot element with the greater element at i
+  swap(&array[i + 1], &array[high]);
+  
+  // return the partition point
+  return (i + 1);
+}
+
+void quickSort(int array[], int low, int high) {
+  if (low < high) {
+    
+    // find the pivot element such that
+    // elements smaller than pivot are on left of pivot
+    // elements greater than pivot are on right of pivot
+    int pi = partition(array, low, high);
+    
+    // recursive call on the left of pivot
+    quickSort(array, low, pi - 1);
+    
+    // recursive call on the right of pivot
+    quickSort(array, pi + 1, high);
+  }
 }
 
 int arrayPairSum(int* nums, int numsSize){
     int temp[numsSize];
     int sum = 0;
-    sort(nums, temp, numsSize);
+    quickSort(nums, 0, numsSize - 1);
     for(int i = 0; i < numsSize; i+=2){
         sum += min(nums[i], nums[i+1]);
     }
