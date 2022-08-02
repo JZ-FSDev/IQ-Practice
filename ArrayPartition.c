83d253bc988393758aadb426f92a132b432560a4
Jason Zhu <94236556+JZ-FSDev@users.noreply.github.com>
2022-08-01 23:42:07 -0500

    Update ArrayPartition.c

--- ArrayPartition.c
+++ ArrayPartition.c
@@ -1,3 +1,9 @@
+// @author JZ-FSDev
+// Runtime: 66 ms, faster than 83.22% of C online submissions for Array Partition.
+
+// Given an integer array nums of 2n integers, group these integers into n pairs (a1, b1), (a2, b2), ..., 
+// (an, bn) such that the sum of min(ai, bi) for all i is maximized. Return the maximized sum.
+
 int min(int a, int b){
     return a <= b ? a : b;
 }
