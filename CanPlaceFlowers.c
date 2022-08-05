5eb535d7ab8a05cc446ea249c1d9bfcd6a29b76e
Jason Zhu <94236556+JZ-FSDev@users.noreply.github.com>
2022-08-04 22:21:25 -0500

    Update CanPlaceFlowers.c

--- CanPlaceFlowers.c
+++ CanPlaceFlowers.c
@@ -2,14 +2,25 @@
 
 bool canPlaceFlowers(int* flowerbed, int flowerbedSize, int n){
     int count = 0;
-    for(int i = 0; i < flowerbedSize; i++){
-        if(i == 0 && flowerbed[i + 1] == 0){
-            count++;
-        }else if(i == flowerbedSize - 1 && flowerbed[i - 1] == 0){
-            count++;
-        }else if(flowerbed[i] != 0 && flowerbed[i + 1] == 0){
-            count++;
+    if(flowerbedSize == 1 && flowerbed[0] == 0){
+        count++;
+    }else{
+        for(int i = 0; i < flowerbedSize; i++){
+            if(flowerbed[i] == 0){
+                if(i == 0 && flowerbed[i + 1] == 0){
+                    count++;
+                    flowerbed[i] = 1;
+                }else 
+                if(i == flowerbedSize - 1 && flowerbed[i - 1] == 0){
+                    count++;
+                    flowerbed[i] = 1;
+                }else 
+                if(i > 0 && i < flowerbedSize - 1 && flowerbed[i + 1] == 0 && flowerbed[i - 1] == 0){
+                    count++;
+                    flowerbed[i] = 1;
+                }
+            }
         }
     }
-    return count == n;
+    return count >= n;
 }
