b36992aa5c00868dbe36b3841979c6b0e2455c24
Jason Zhu <94236556+JZ-FSDev@users.noreply.github.com>
2022-08-05 22:59:54 -0500

    Update CanPlaceFlowers.c

--- CanPlaceFlowers.c
+++ CanPlaceFlowers.c
@@ -1,4 +1,10 @@
 
+// @author JZ-FSDev
+// Runtime: 17 ms, faster than 96.24% of C online submissions for Can Place Flowers.
+
+// You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in adjacent plots.
+
+// Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n, return if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule.
 
 bool canPlaceFlowers(int* flowerbed, int flowerbedSize, int n){
     int count = 0;
