50541397d2b9a2dfc8fc7409e5c652b38a4bfa52
Jason Zhu <94236556+JZ-FSDev@users.noreply.github.com>
2022-08-14 23:27:22 -0500

    Update SetMismatch.c

--- SetMismatch.c
+++ SetMismatch.c
@@ -7,10 +7,13 @@ int* findErrorNums(int* nums, int numsSize, int* returnSize){
     int arr[numsSize];
     int num1 = 0;
     int num2 = 0;
-    int *ret = malloc(sizeof(int) * 2)
+    for(int i = 0; i < numsSize; i++){
+        arr[i] = -1;
+    }
+    int *ret = malloc(sizeof(int) * 2);
     for(int i = 0; i < numsSize; i++){
         if(arr[nums[i] -1] == 1){
-            arr[nums[i] -1] = 2;
+            arr[nums[i] - 1] = 2;
         }else{
             arr[nums[i] -1] = 1;
         }
@@ -18,11 +21,13 @@ int* findErrorNums(int* nums, int numsSize, int* returnSize){
     }
     for(int i = 0; i < numsSize; i++){
         if(arr[i] == 2){
-            num1 = arr[i];
-        }else if(arr[i] != 1){
-            num2 = arr[i];
+            num1 = i + 1;
+        }else if(arr[i] == -1){
+            num2 = i + 1;
         }
     }
     ret[0] = num1;
     ret[1] = num2;
+    *returnSize = 2;
+    return ret;
 }
