687b3737437a34622d4771d9a4309d04d8d4a892
Jason Zhu <94236556+JZ-FSDev@users.noreply.github.com>
2022-08-18 23:48:15 -0500

    Update ValidPalindrome2.c

--- ValidPalindrome2.c
+++ ValidPalindrome2.c
@@ -1,5 +1,27 @@
 
+bool solve(int i, int j, char *s){
+    while(j > i){
+        if(s[i] != s[j]){
+            return false;
+        }
+        j--;
+        i++;
+    }
+    return true;
+}
 
 bool validPalindrome(char * s){
-
+    int count = 0;
+    int j = strlen(s) - 1;
+    int i = 0;
+    while(j > i){
+        if(s[i] != s[j]){
+            count++;
+            return solve(i+1, j, s) || solve(i, j-1, s);
+        }
+        j--;
+        i++;
+    }
+    return true;
 }
+
