ac1bc022c335112bc208e34d2874ffc3b6944d59
Jason Zhu <94236556+JZ-FSDev@users.noreply.github.com>
2022-08-21 23:19:44 -0500

    Update BinaryNumberWithAlternatingBits.c

--- BinaryNumberWithAlternatingBits.c
+++ BinaryNumberWithAlternatingBits.c
@@ -1,4 +1,12 @@
-
 bool hasAlternatingBits(int n){
-
+    int curr = n % 2;
+    n /= 2; // shift bits to right
+    while(n > 0){
+        if(curr == n % 2){
+            return false;
+        }
+        curr = n % 2;
+        n /= 2;
+    }
+    return true;
 }
