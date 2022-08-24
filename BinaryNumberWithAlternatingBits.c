b7a641b6bcd4e767579bffd9eb17449109a0dc64
Jason Zhu <94236556+JZ-FSDev@users.noreply.github.com>
2022-08-23 23:54:54 -0500

    Update BinaryNumberWithAlternatingBits.c

--- BinaryNumberWithAlternatingBits.c
+++ BinaryNumberWithAlternatingBits.c
@@ -1,3 +1,8 @@
+// @author JZ-FSDev
+// Runtime: 0 ms, faster than 100.00% of C online submissions for Binary Number with Alternating Bits.
+
+// Given a positive integer, check whether it has alternating bits: namely, if two adjacent bits will always have different values.
+
 bool hasAlternatingBits(int n){
     int curr = n % 2;
     n /= 2; // shift bits to right
