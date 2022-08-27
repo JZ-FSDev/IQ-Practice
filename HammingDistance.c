344d262cef2123c329dcc90ce6eefe91b1197573
Jason Zhu <94236556+JZ-FSDev@users.noreply.github.com>
2022-08-26 22:57:15 -0500

    Update HammingDistance.c

--- HammingDistance.c
+++ HammingDistance.c
@@ -1,4 +1,8 @@
+// @author JZ-FSDev
+// Runtime: 0 ms, faster than 100.00% of C online submissions for Hamming Distance.
 
+// The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
+// Given two integers x and y, return the Hamming distance between them.
 
 int hammingDistance(int x, int y){
     int xCurr = x % 2, yCurr = y % 2;
