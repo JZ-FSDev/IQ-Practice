9aaebb82accf4506b19aab63b050fe38903299c8
Jason Zhu <94236556+JZ-FSDev@users.noreply.github.com>
2022-08-29 23:33:11 -0500

    Update Base7.c

--- Base7.c
+++ Base7.c
@@ -1,56 +1,29 @@
 
+int ind = 0;
+char ptr[12];
+
+
+void convert(int num){
+    if(num != 0){
+        convert(num / 7);
+        ptr[ind++] = '0' + (num - (num / 7) * 7);
+    }
+}
 
 char * convertToBase7(int num){
-    int result = 0;
-    int resultTemp = 0;
-    int digits = 1;
-    char *ptr;
+    ind = 0;
+    for(int i = 0; i < 12; i++){
+        ptr[i] = '\0';
+    }
+    if(num < 0){
+        ptr[ind++] = '-';
+        num *= -1;
+    }
     if(num > 0){
-        while(num > 0){
-            if(num >= 7){
-                num -= 7;
-                result += 10;
-            }else{
-                result += num;
-                num = 0;
-            }
-        }
+        convert(num);
     }else{
-        while(num < 0){
-            if(num <= -7){
-                num += 7;
-                result -= 10;
-            }else{
-                result -= num;
-                num = 0;
-            }
-        }
+        ptr[ind++] = '0';
     }
-    resultTemp = result;
-    while(result > 0){
-        digits++;
-        result /= 10;
-    }
-    ptr = malloc(digits + 3);
-    if(ptr){
-        int index = 0;
-        if(resultTemp != 0){
-            if(resultTemp < 0){
-                ptr[index++] = '-';
-            }
-            while(resultTemp != 0){
-                if(resultTemp == 10){
-                    ptr[index++] = '1';
-                    ptr[index++] = '0';
-                }else{
-                    ptr[index++] = '0' + (resultTemp % 10);
-                }
-                resultTemp /= 10;
-            }
-        }else{
-            ptr[index++] = '0';
-        }
-        ptr[index] = '\0';
-    }
+    ptr[ind] = '\0';
     return ptr;
 }
