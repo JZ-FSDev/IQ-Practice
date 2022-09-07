db2079466ef5592ce289ae0377fcf42a5a81c2a2
Jason Zhu <94236556+JZ-FSDev@users.noreply.github.com>
2022-09-06 23:43:44 -0500

    Update LongestHarmoniousSubsequence.java

--- LongestHarmoniousSubsequence.java
+++ LongestHarmoniousSubsequence.java
@@ -1,3 +1,21 @@
-int findLHS(int* nums, int numsSize){
-
+class Solution {
+    public int findLHS(int[] nums) {
+        Arrays.sort(nums);
+        int len = 0;
+        int temp = 0;
+        for(int i = 0; i < nums.length; i++){
+            temp = 0;
+            for(int j = i + 1; j < nums.length; j++){
+                if(Math.abs(nums[j] - nums[i]) == 1){
+                    temp++;
+                }else{
+                    break;
+                }    
+            }
+            if(temp > len){
+                len = temp;
+            }
+        }
+        return len;
+    }
 }
