15e451561262afad70b373a4aba4755428d6d4fa
JZ-FSDev <jzcompsci@gmail.com>
2022-09-13 22:10:39 -0500

    Update LongestHarmoniousSubsequence.java

--- LongestHarmoniousSubsequence.java
+++ LongestHarmoniousSubsequence.java
@@ -1,3 +1,10 @@
+// @author JZ-FSDev
+// Runtime: 1476 ms, faster than 5.06% of Java online submissions for Longest Harmonious Subsequence.
+
+// We define a harmonious array as an array where the difference between its maximum value and its minimum value is exactly 1.
+// Given an integer array nums, return the length of its longest harmonious subsequence among all its possible subsequences.
+// A subsequence of array is a sequence that can be derived from the array by deleting some or no elements without changing the order of the remaining elements.
+
 class Solution {
     public int findLHS(int[] nums) {
         Arrays.sort(nums);
