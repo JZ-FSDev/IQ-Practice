441e391a2df3a2f5559316e6d352e3d6b58be022
JZ-FSDev <jzcompsci@gmail.com>
2022-09-21 23:55:21 -0500

    Update Largest3-Same-DigitNumberInString.java

--- Largest3-Same-DigitNumberInString.java
+++ Largest3-Same-DigitNumberInString.java
@@ -1,27 +1,26 @@
+// @author JZ-FSDev
+// Runtime: 12 ms, faster than 22.11% of Java online submissions for Largest 3-Same-Digit Number in String.
+
+// You are given a string num representing a large integer. An integer is good if it meets the following conditions:
+// - It is a substring of num with length 3.
+// - It consists of only one unique digit.
+// Return the maximum good integer as a string or an empty string "" if no such integer exists.
+
 public class Largest3-Same-DigitNumberInString {
     class Solution {
         public String largestGoodInteger(String num) {
             String ret = "";
-            char sub = 'a';
-            boolean repeat = true;
-            for(int i = 0; i < num.length() - 2; i++){
-                sub = num.charAt(i);
-                for(int j = i + 1; j < num.length() && j <= i + 2; j++){
-                    if(num.charAt(j) != sub){
-                        repeat = false;
-                        if(i == num.length() - 3){
-                            return "";
-                        }
-                        break;
+            char sub = '0';
+            boolean found = false;
+            for(int i = 2; i < num.length(); i++){
+                if(num.charAt(i - 1) == num.charAt(i) && num.charAt(i - 1) == num.charAt(i - 2)){
+                    if(num.charAt(i - 1) > sub){
+                        sub = num.charAt(i - 1);
                     }
-                }
-                if(repeat){
-                    break;
-                }else{
-                    repeat = true;
+                    found = true;
                 }
             }
-            if(repeat){
+            if(found){
                 ret += sub;
                 ret += sub;
                 ret += sub;
