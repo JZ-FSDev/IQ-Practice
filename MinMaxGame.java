210c6b11b0cfe615e160e285d1a7a2a25272f63c
JZ-FSDev <jzcompsci@gmail.com>
2022-09-29 23:43:42 -0500

    Update MinMaxGame.java

--- MinMaxGame.java
+++ MinMaxGame.java
@@ -1,7 +1,14 @@
 public class MinMaxGame {
     class Solution {
         public int minMaxGame(int[] nums) {
-            
+            while(nums.length > 1){
+                int[] temp = new int[nums.length / 2];
+                for(int i = 0; i < temp.length; i++){
+                    if(i % 2 == 0){
+                        
+                    }
+                }
+            }
         }
     }
 }
