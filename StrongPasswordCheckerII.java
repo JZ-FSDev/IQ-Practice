0c142265c01f70617b6e5fef6bdee4739ea46ffe
JZ-FSDev <jzcompsci@gmail.com>
2022-10-06 22:54:46 -0500

    Update StrongPasswordCheckerII.java

--- StrongPasswordCheckerII.java
+++ StrongPasswordCheckerII.java
@@ -1,3 +1,16 @@
+// @author JZ-FSDev
+// Runtime: Runtime: 1 ms, faster than 98.05% of Java online submissions for Strong Password Checker II.
+
+// A password is said to be strong if it satisfies all the following criteria:
+
+// It has at least 8 characters.
+// It contains at least one lowercase letter.
+// It contains at least one uppercase letter.
+// It contains at least one digit.
+// It contains at least one special character. The special characters are the characters in the following string: "!@#$%^&*()-+".
+// It does not contain 2 of the same character in adjacent positions (i.e., "aab" violates this condition, but "aba" does not).
+// Given a string password, return true if it is a strong password. Otherwise, return false.
+
 public class StrongPasswordCheckerII {
     class Solution {
         public boolean strongPasswordCheckerII(String password) {
