fd1f86b5500c0bc1fb67898fc7037d4719e85e90
JZ-FSDev <jzcompsci@gmail.com>
2022-10-08 23:46:19 -0500

    Update EvaluateBooleanBinaryTree.java

--- EvaluateBooleanBinaryTree.java
+++ EvaluateBooleanBinaryTree.java
@@ -16,7 +16,21 @@ public class EvaluateBooleanBinaryTree {
      */
     class Solution {
         public boolean evaluateTree(TreeNode root) {
-
+            if (root != null && root.left != null) {
+                boolean left = evaluateTree(root.left);
+                boolean right = evaluateTree(root.right);
+                if (root.val == 2) {
+                    return left || right;
+                } else {
+                    return left && right;
+                }
+            } else {
+                if (root.val == 1) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
         }
     }
 }
