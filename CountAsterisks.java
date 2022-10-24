14e06db543df4e6a4b5970a15211012de2e2406e
JZ-FSDev <jzcompsci@gmail.com>
2022-10-23 23:27:12 -0500

    Update CountAsterisks.java

--- CountAsterisks.java
+++ CountAsterisks.java
@@ -1,7 +1,17 @@
 public class CountAsterisks {
     class Solution {
         public int countAsterisks(String s) {
-            
+            int count = 0;
+            int status = 0;
+            for(int i = 0; i < s.length(); i++){
+                if(s.charAt(i) == '|'){
+                    status++;
+                }
+                if(s.charAt(i) == '*' && status % 2 == 0){
+                    count++;
+                }
+            }
+            return count;
         }
     }
 }
