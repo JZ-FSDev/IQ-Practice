c4f57a98468bcfcc0c4def9c797c17706d378ec5
JZ-FSDev <jzcompsci@gmail.com>
2022-10-24 23:41:35 -0500

    Update CountAsterisks.java

--- CountAsterisks.java
+++ CountAsterisks.java
@@ -1,3 +1,11 @@
+// @author JZ-FSDev
+// Runtime: 3 ms, faster than 66.36% of Java online submissions for Count Asterisks.
+
+// You are given a string s, where every two consecutive vertical bars '|' are grouped into a pair. In other words, the 1st and 2nd '|' make a pair, the 3rd and 4th '|' make a pair, and so forth.
+
+// Return the number of '*' in s, excluding the '*' between each pair of '|'.
+
+// Note that each '|' will belong to exactly one pair.
 public class CountAsterisks {
     class Solution {
         public int countAsterisks(String s) {
