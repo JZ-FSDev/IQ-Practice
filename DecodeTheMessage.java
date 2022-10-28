4197704ba4dbe6a7eb4c0a1fc17fefdbb35fb81b
JZ-FSDev <jzcompsci@gmail.com>
2022-10-27 23:54:14 -0500

    Update DecodeTheMessage.java

--- DecodeTheMessage.java
+++ DecodeTheMessage.java
@@ -1,7 +1,23 @@
 public class DecodeTheMessage {
     class Solution {
         public String decodeMessage(String key, String message) {
-            
+            StringBuilder s = new StringBuilder();
+            HashMap<Character, Character> dict = new HashMap<Character, Character>();
+            int pos = 0;
+            for(int i = 0; i < key.length() && dict.size() < 26; i++){
+                if(dict.get(key.charAt(i)) == null){
+                    dict.put(key.charAt(i), (char)('a' + pos));
+                    pos++;
+                }
+            }
+            for(int i = 0; i < message.length(); i++){
+                if(message.charAt(i) == ' '){
+                    s.append(" ");
+                }else{
+                    s.append(dict.get(i));
+                }
+            }
+            return s.toString();
         }
     }
 }
