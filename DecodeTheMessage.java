ca39bd08d221073d122b27c189ceb19b9b34136b
JZ-FSDev <jzcompsci@gmail.com>
2022-11-03 00:19:40 -0500

    Update DecodeTheMessage.java

--- DecodeTheMessage.java
+++ DecodeTheMessage.java
@@ -1,12 +1,24 @@
+// @author JZ-FSDev
+// Runtime: 12 ms, faster than 71.97% of Java online submissions for Decode the Message.
+
+// You are given the strings key and message, which represent a cipher key and a secret message, respectively. The steps to decode message are as follows:
+
+// Use the first appearance of all 26 lowercase English letters in key as the order of the substitution table.
+// Align the substitution table with the regular English alphabet.
+// Each letter in message is then substituted using the table.
+// Spaces ' ' are transformed to themselves.
+// For example, given key = "happy boy" (actual key would have at least one instance of each letter in the alphabet), we have the partial substitution table of ('h' -> 'a', 'a' -> 'b', 'p' -> 'c', 'y' -> 'd', 'b' -> 'e', 'o' -> 'f').
+// Return the decoded message.
+
 public class DecodeTheMessage {
     class Solution {
         public String decodeMessage(String key, String message) {
             StringBuilder s = new StringBuilder();
-            HashMap<String, String> dict = new HashMap<String, String>();
+            HashMap<Character, Character> dict = new HashMap<Character, Character>();
             int pos = 0;
             for(int i = 0; i < key.length() && dict.size() < 26; i++){
-                if(dict.get("" + key.charAt(i)) == null){
-                    dict.put("" + key.charAt(i), "" + ((char)('a' + pos)));
+                if(key.charAt(i) != ' ' && dict.get(key.charAt(i)) == null){
+                    dict.put(key.charAt(i), ((char)('a' + pos)));
                     pos++;
                 }
             }
@@ -14,7 +26,7 @@ public class DecodeTheMessage {
                 if(message.charAt(i) == ' '){
                     s.append(" ");
                 }else{
-                    s.append(dict.get("" + message.charAt(i)));
+                    s.append("" + dict.get(message.charAt(i)));
                 }
             }
             return s.toString();
